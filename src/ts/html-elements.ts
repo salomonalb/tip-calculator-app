c0ad5467fbdca2e8aa6735e1238970fec094b61c
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-11-08 20:31:15 -0400

    elements variables module

---
+++ src/ts/html-elements.ts
@@ -0,0 +1,24 @@
+const app = document.querySelector('#app');
+
+// Text Inputs
+const billInput = document.querySelector('#bill');
+const customInput = document.querySelector('#custom');
+const peopleInput = document.querySelector('#numOfPeople');
+
+// Buttons
+const button5 = document.querySelector('#5');
+const button10 = document.querySelector('#10');
+const button15 = document.querySelector('#15');
+const button25 = document.querySelector('#25');
+const button50 = document.querySelector('#50');
+const buttons = [button5, button10, button15, button25, button50];
+const resetButton = document.querySelector('#reset')
+
+// Outputs
+const tipOutput = document.querySelector('#tipAmount');
+const totalOutput = document.querySelector('#total');
+
+// Alerts
+const billAlert = document.querySelector('#billAlert');
+const customAlert = document.querySelector('#customAlert');
+const peopleAlert = document.querySelector('#peopleAlert');

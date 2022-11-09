85b55704c12d4d59e937a8284fd6d494a70e7bff
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-11-08 23:11:33 -0400

    imports and calls to validate

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,148 +1,32 @@
-"use strict"
-import './html-elements'
-import './value-variables'
+import * as el from './html-elements';
+import * as val from './value-variables';
+import  validate  from './validate'
 
-function clearCustomInput() {
-    customInput.value = '';
-    customInput.classList.remove('--valid');
-    customInput.classList.remove('--invalid');
-    customAlert.style.display = 'none';
-    customAlert.textContent = ``;
-}
+//import the function, now eberthing you export and import has to be namespaced
 
-function clearOutput() {
-    tipOutput.textContent = `$0.00`;
-    totalOutput.textContent = `$0.00`;
-}
-
-function calculation() {
-
-    billValue = Number.parseInt(billValue);
-    percentageValue = Number.parseInt(percentageValue);
-    peopleValue = Number.parseInt(peopleValue);
-
-    console.log(typeof billValue + " " + billValue)
-    console.log(typeof percentageValue + " " + percentageValue);
-    console.log(typeof peopleValue + " " + peopleValue);
-
-
-    const tipAmount = ((billValue * percentageValue) / 100) / peopleValue || 0;
-
-    const totalAmount = (billValue + ((billValue * percentageValue) / 100)) / peopleValue || 0; 
-    
-
-    if (tipAmount === NaN || tipAmount === Infinity || tipAmount === 0 ||totalAmount === NaN || totalAmount === Infinity  ) {
-
-        clearOutput()
-        
-    } else {
-
-        console.log('tip amount ' + typeof tipAmount + ` ${tipAmount}`);
-
-        tipOutput.textContent = `$${tipAmount.toFixed(2)}`;
-        totalOutput.textContent = `$${totalAmount.toFixed(2)}`;
-    }
-}
-
-function billValidate() {
-    
-    if (billInput.validity.valid === false || billInput.value === "" || billInput.value < 1) {
-        billInput.classList.remove('--valid');
-        billInput.classList.add('--invalid');
-        billAlert.style.display = 'inline-block';
-        billAlert.textContent = `Only numbers, Can't be zero, up to two decimal points`;
-
-        billValue = 0;
-        clearOutput()
-
-        
-
-    } else  {
-        billInput.classList.remove('--invalid');
-        billInput.classList.add('--valid');
-        billAlert.style.display = 'none';
-        billAlert.textContent = ``;
-
-        billValue = billInput.value;
-        calculation()
-    }
-}
-
-function customValidate() {
-    if (customInput.validity.valid === false || customInput.value === "" || customInput.value < 1) {
-        customInput.classList.remove('--valid');
-        customInput.classList.add('--invalid');
-        customAlert.style.display = 'block';
-        customAlert.textContent = `Can't be zero, Only whole numbers up to three digits`;
-
-        
-        percentageValue = 0;
-        clearOutput()
-
-    } else {
-        customInput.classList.remove('--invalid');
-        customInput.classList.add('--valid');
-        customAlert.style.display = 'none';
-        customAlert.textContent = ``;
-
-        percentageValue = customInput.value;
-        calculation()
-    }
-
-    const otherButtons = [button5, button10, button15, button25, button50];
-    otherButtons.forEach((button) => {
-        button.classList.remove('--selected')
-
-    })
-
-    
-}
-function peopleValidate() {
-    if (peopleInput.validity.valid === false || peopleInput.value === "" || peopleInput.value < 1) {
-
-        peopleInput.classList.remove('--valid');
-        peopleInput.classList.add('--invalid');
-        peopleAlert.style.display = 'block';
-        peopleAlert.textContent = `Can't be zero, Only whole numbers up to three digits`;
-
-        
-        peopleValue = 0;
-        clearOutput()
-
-    } else {
-        peopleInput.classList.remove('--invalid');
-        peopleInput.classList.add('--valid');
-        peopleAlert.style.display = 'none';
-        peopleAlert.textContent = ``;
-
-        peopleValue = peopleInput.value;
-        calculation()
-    }
-}
-
-
-
-// you can use the same function and logic with a function where you pass the input, the alert and a string for it, the value, and other functon for the radio button
-
-
-
-
-app.addEventListener('submit',  (event)=> {
+// Events
+el.app.addEventListener('submit',  (event)=> {
     event.preventDefault()
 })
 
-billInput.addEventListener('input', ()=> {
-    billValidate()
+el.billInput.addEventListener('input', ()=> {
+    validate(el.billInput, el.billAlert, val.billValue, el.billMessage)
 })
 
-customInput.addEventListener('input', ()=> {
-    customValidate()
+el.customInput.addEventListener('input', ()=> {
+    validate(el.customInput, el.customAlert, val.percentageValue, el.customMessage)
+
+    el.buttons.forEach((button) => {
+        button.classList.remove('--selected')
+    })
 })
 
-peopleInput.addEventListener('input', ()=> {
-    peopleValidate()
+el.peopleInput.addEventListener('input', ()=> {
+    validate(el.peopleInput, el.peopleAlert, val.peopleValue, el.peopleMessage)
 })
 
+
+/*
 buttons.forEach((button) => {
     
     button.addEventListener('click', () => {
@@ -185,4 +69,4 @@ resetButton.addEventListener('click', event => {
     billAlert.style.display = 'none';
     billAlert.textContent = ``;
 })
-
+*/
